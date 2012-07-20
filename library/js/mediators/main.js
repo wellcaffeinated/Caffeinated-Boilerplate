e0aef0b3dc9041426a477042ba608ffab47c8315
Jasper Palfree <well.caffeinated@gmail.com>
2012-07-20 11:05:25 -0400

    Reworked to be simple frontend bp

---
+++ library/js/mediators/main.js
@@ -0,0 +1,22 @@
+define(
+    [
+        'jquery',
+        'stapes'
+    ],
+    function(
+        $,
+        Stapes
+    ){
+        'use strict';
+
+        var mediator = Stapes.create().extend({
+
+            init: function(){
+
+                // init
+            }
+        });
+        
+        mediator.init();
+    }
+);
