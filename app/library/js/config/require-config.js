bd60f57994e50a7395ddfae63c3d9248a49a3fe0
Jasper <wellcaffeinated@iCoffee.local>
2013-05-03 18:48:55 -0400

    added flat-ui assets

--- app/library/js/config/require-config.js
+++ app/library/js/config/require-config.js
@@ -5,6 +5,24 @@ require.config({
     
     shim: {
         // Add shims for things here
+        'jquery-ui': {
+            deps: ['jquery']
+        },
+        'bootstrap-tooltip': {
+            deps: ['jquery']
+        },
+        'jquery.dropkick': {
+            deps: ['jquery']
+        },
+        'jquery.tagsinput': {
+            deps: ['jquery']
+        },
+        'custom-checkbox': {
+            deps: ['jquery']
+        },
+        'custom-radio': {
+            deps: ['jquery']
+        }
     },
 
     paths: {
@@ -25,13 +43,19 @@ require.config({
         'stapes': 'vendor/stapes',
         
         // jQuery
-        'jquery': 'vendor/jquery'
+        'jquery': 'vendor/jquery',
+        'jquery-ui': 'vendor/jquery-ui-1.10.0.custom.min',
+        'jquery.dropkick': 'vendor/jquery.dropkick-1.0.0',
+        'bootstrap-tooltip': 'vendor/bootstrap-tooltip',
+        'jquery.tagsinput': 'vendor/jquery.tagsinput',
+        'custom-checkbox': 'vendor/flat-ui/custom_checkbox_and_radio',
+        'custom-radio': 'vendor/flat-ui/custom_radio'
     },
 
     map: {
         
         '*' : {
-            'jquery': 'modules/adapters/jquery', // jQuery noconflict adapter
+            // 'jquery': 'modules/adapters/jquery', // jQuery noconflict adapter
             'site-config': 'config/site-config.json'
         },
 
--- app/library/js/mediators/boilerplate.js
+++ app/library/js/mediators/boilerplate.js
@@ -2,12 +2,24 @@ define(
     [
         'jquery',
         'stapes',
-        './globals'
+        './globals',
+        'jquery-ui',
+        'bootstrap-tooltip',
+        'jquery.dropkick',
+        'jquery.tagsinput',
+        'custom-checkbox',
+        'custom-radio'
     ],
     function(
         $,
         Stapes,
-        globals
+        globals,
+        _jqui,
+        _bstt,
+        _dk,
+        _jqtags,
+        _cc,
+        _cr
     ) {
 
         'use strict';
@@ -51,8 +63,49 @@ define(
              */
             onDomReady : function(){
 
-                var self = this;
+                // Custom selects
+                $("select").dropkick();
+            
+                // Todo list
+                $(".todo li").click(function() {
+                    $(this).toggleClass("todo-done");
+                });
 
+                // // Init tooltips
+                $("[data-toggle=tooltip]").tooltip("show");
+
+                // Init tags input
+                $("#tagsinput").tagsInput();
+
+                // Init jQuery UI slider
+                $("#slider").slider({
+                    min: 1,
+                    max: 5,
+                    value: 2,
+                    orientation: "horizontal",
+                    range: "min",
+                });
+
+                // JS input/textarea placeholder
+                // $("input, textarea").placeholder();
+
+                // Make pagination demo work
+                $(".pagination a").click(function() {
+                    if (!$(this).parent().hasClass("previous") && !$(this).parent().hasClass("next")) {
+                        $(this).parent().siblings("li").removeClass("active");
+                        $(this).parent().addClass("active");
+                    }
+                });
+
+                $(".btn-group a").click(function() {
+                    $(this).siblings().removeClass("active");
+                    $(this).addClass("active");
+                });
+
+                // Disable link click not scroll top
+                $("a[href='#']").click(function() {
+                    return false
+                });
             }
 
         });
